7243cfaa5ae7b886a6820825be5f8b3b99f844d4
[lutianqi] <[1179555783@qq.com]>
2018-05-14 17:03:38 +0800

    1

--- src/main/java/com/dubbo/controller/TestController.java
+++ src/main/java/com/dubbo/controller/TestController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("test")
 public class TestController {
@@ -14,8 +17,10 @@ public class TestController {
 
 
     @RequestMapping("eat")
-    public String eat() {
-        testDubboService.eat();
-        return "11";
+    public Map<String, Object> eat() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        String eat = testDubboService.eat();
+        map.put("eat", eat);
+        return map;
     }
 }
